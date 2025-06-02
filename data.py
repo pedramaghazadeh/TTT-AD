Adding 4 types of rotation and minor bug fixes

data.py
@@ -18,8 +18,8 @@ SCENE2ID = {k: i for i, k in enumerate(SCENE_LABELS)}
 ID2SCENE = {i: k for i, k in enumerate(SCENE_LABELS)}
 
 class BDDDualTaskDataset(Dataset):
-    def __init__(self, root_path, parition="train", input_size=224):
-        self.root = os.path.join(root_path, parition + '/')
+    def __init__(self, root_path, partition="train", input_size=224):
+        self.root = os.path.join(root_path, partition + '/')
         self.input_size = input_size
         assert os.path.exists(self.root), f"Data directory {self.root} does not exist."
         print(f"Loading dataset from {self.root}")
@@ -50,7 +50,7 @@ class BDDDualTaskDataset(Dataset):
             T.Normalize(mean=mean, std=std)
         ])
 
-        print("Total data available is", len(self.samples))
+        print(f"Total data available is {len(self.samples)} images in {partition} parition", )
 
     def __len__(self):
         return len(self.samples)
@@ -58,7 +58,9 @@ class BDDDualTaskDataset(Dataset):
     def __getitem__(self, idx):
         image, label = self.samples[idx]
         # Random rotation
-        angle = random.uniform(0, 360)
+        # angle = random.uniform(0, 360)
+        angle = random.choice([0, 90, 180, 270]) # For simplicity, we can limit to these angles
+        labels_angle = {0: 0, 90: 1, 180: 2, 270: 3}
         rotated = image.rotate(angle, resample=Image.BILINEAR)
 
         image_tensor = self.transform(image)
@@ -67,7 +69,7 @@ class BDDDualTaskDataset(Dataset):
         return {"image": image_tensor, 
                 "label": torch.tensor(label), 
                 "image_rot": image_rot_tensor, 
-                "angle": torch.tensor([angle / 360], dtype=torch.float32) # Normalized angle to [0, 1]
+                "angle": torch.tensor(labels_angle[angle]) # Normalized angle to [0, 1]
                 }
 
 

main.py
@@ -19,14 +19,14 @@ def train_model(args):
     wandb.define_metric("test/*", step_metric="test/step")
     wandb.define_metric("ttt/*", step_metric="ttt/step")
 
-    dataset = BDDDualTaskDataset(root_path=args.data_path, parition="train")
+    dataset = BDDDualTaskDataset(root_path=args.data_path, partition="train")
     train_dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=32)
 
     # Validation dataset
-    val_dataset = BDDDualTaskDataset(root_path=args.data_path, parition="val")
+    val_dataset = BDDDualTaskDataset(root_path=args.data_path, partition="val")
     val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=32)
     # Test dataset
-    test_dataset = BDDDualTaskDataset(root_path=args.data_path, parition="test")
+    test_dataset = BDDDualTaskDataset(root_path=args.data_path, partition="test")
     test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=32)
 
     print(f"Train dataset size: {len(dataset)}")
@@ -35,7 +35,7 @@ def train_model(args):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-    for _ in tqdm(range(10), desc="Training and validating"):
+    for _ in tqdm(range(args.num_epochs), desc="Training and validating"):
         # Training
         train_epoch(model, train_dataloader, optimizer, device)
         # Validation
@@ -46,14 +46,16 @@ def train_model(args):
 
     # TTT on test dataset
     for partition in CORRUPTED:
-        corrupted_dataset = BDDDualTaskDataset(root_path=args.data_path, parition=partition, batch_size=1, num_samples=1000)
-        test_time_training_inference(model, corrupted_dataset, device, partition)
+        corrupted_dataset = BDDDualTaskDataset(root_path=args.data_path, partition=partition, num_samples=1000)
+        corrupted_dataloader = DataLoader(corrupted_dataset, batch_size=1, shuffle=False, num_workers=32)
+        test_time_training_inference(model, corrupted_dataloader, device, partition)
     # torch.save(model.state_dict(), f"model_.pth")
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train EfficientNet for TTT")
     parser.add_argument('--device', type=int, default=0, help='GPU device ID')
     parser.add_argument('--data-path', type=str, default='/scr/Pedram/VisualLearning/processed_bdd100k/', help='Path to the dataset')
+    parser.add_argument('--num-epochs', type=int, default=2, help='Number of training epochs')
 
     args = parser.parse_args()
 

model.py
@@ -18,14 +18,14 @@ class EfficientNetTTT(nn.Module):
             self.rotation_head = nn.Sequential(
                 nn.Linear(self.feature_dim_rot, 128),
                 nn.ReLU(),
-                nn.Linear(128, 1)  # Regression: rotation in degrees
+                nn.Linear(128, 4), # Classification: 4 classes for 0, 90, 180, 270 degrees
             )
         else:
             self.encoder_rotation = self.encoder
             self.rotation_head = nn.Sequential(
                 nn.Linear(self.feature_dim, 128),
                 nn.ReLU(),
-                nn.Linear(128, 1)  # Regression: rotation in degrees
+                nn.Linear(128, 4), # Classification: 4 classes for 0, 90, 180, 270 degrees
             )
 
     def forward(self, x):
@@ -35,9 +35,3 @@ class EfficientNetTTT(nn.Module):
         feats_rot = self.encoder_rotation(x)
         rot_pred = self.rotation_head(feats_rot)
         return cls_logits, rot_pred
-
-# def extractor_from_layer4(net):
-# 	layers = [net.conv1, net.bn1, net.relu, net.maxpool,
-# 				 net.layer1, net.layer2, net.layer3, net.layer4, 
-# 					net.avgpool, ViewFlatten()]
-# 	return nn.Sequential(*layers)

train.py
@@ -13,8 +13,8 @@ def train_epoch(model, dataloader, optimizer, device, ttt=True):
     model.train()
     cls_loss_fn = nn.CrossEntropyLoss()
 
-    rot_loss_fn = nn.MSELoss()
-    # rot_loss_fn = nn.CrossEntropyLoss()
+    # rot_loss_fn = nn.MSELoss()
+    rot_loss_fn = nn.CrossEntropyLoss()
 
     total_cls_loss, total_rot_loss = 0, 0
     total_acc = 0
@@ -56,7 +56,9 @@ def train_epoch(model, dataloader, optimizer, device, ttt=True):
 def validate_epoch(model, dataloader, device):
     model.eval()
     cls_loss_fn = nn.CrossEntropyLoss()
-    rot_loss_fn = nn.MSELoss()
+    # rot_loss_fn = nn.MSELoss()
+    rot_loss_fn = nn.CrossEntropyLoss()
+
     total_cls_loss, total_rot_loss = 0, 0
     total_acc = 0
     global val_step
@@ -84,7 +86,9 @@ def validate_epoch(model, dataloader, device):
 def test_epoch(model, dataloader, device):
     model.eval()
     cls_loss_fn = nn.CrossEntropyLoss()
-    rot_loss_fn = nn.MSELoss()
+    # rot_loss_fn = nn.MSELoss()
+    rot_loss_fn = nn.CrossEntropyLoss()
+
     total_cls_loss, total_rot_loss = 0, 0
     total_acc = 0
     global test_step
@@ -115,7 +119,9 @@ def test_time_training_inference(model, dataloader, device, num_iterations=100):
     ttt_model.train()
     acc = 0
     optimizer = torch.optim.Adam(ttt_model.parameters(), lr=1e-4)
-    rot_loss_fn = nn.MSELoss()
+    # rot_loss_fn = nn.MSELoss()
+    rot_loss_fn = nn.CrossEntropyLoss()
+
     global ttt_step
     for batch in dataloader:
         # Batch size of 1